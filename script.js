f106e2b3a7663478e6cc28ee5c1b6ccf68dd191d
P C Uma Mahesh <117726551+Hi-TechMissile@users.noreply.github.com>
2023-02-01 22:49:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
-let gold = 50;
+let gold = 20;
 let health = 100;
 let currentWeapon = 0;
 let xp = 0;
 let hps = 50;
 let hpb = 100;
 let hpd = 200;
+let mons = 0;
+let power = 10;
+let def;
+let bef;
+let man;
+let t;
+let temp;
 const button1 = document.querySelector("#button1");
 const button2 = document.querySelector("#button2");
 const button3 = document.querySelector("#button3");
@@ -21,7 +28,21 @@ const hard = document.querySelector('#hard');
 const insane = document.querySelector('#insane');
 button1.onclick = goStore;
 button2.onclick = goCave;
-button3.onlcick = fightDragon;
+button3.onclick = fightDragon;
+const m = [
+    {
+        name: "Slime",
+        mh: 50
+    },
+    {
+        name: "Beast",
+        mh: 100
+    },
+    {
+        name: "Dragon",
+        mh: 200
+    }
+];
 const weapons = [
     {
         name: "Stick",
@@ -70,6 +91,7 @@ function home()
     button1.onclick = goStore;
     button2.onclick = goCave;
     button3.onclick = fightDragon;
+    slime.display.style = "none"
 }
 function buyhealth()
 {
@@ -81,7 +103,7 @@ function buyhealth()
         g.innerText = gold;
     }
     else
-        text.innerText = "Not enough Gold!";    
+        text.innerText = "Not enough Gold to buy Health!";    
 }
 function buyweapon()
 {
@@ -92,6 +114,7 @@ function buyweapon()
         currentWeapon = 4
         stat.innerText = weapons[currentWeapon].name;
         p.innerText = weapons[currentWeapon].power;
+        power = weapons[currentWeapon].power;
     }
     else if(gold>=60)
     {
@@ -100,6 +123,7 @@ function buyweapon()
         g.innerText = gold
         stat.innerText = weapons[currentWeapon].name;
         p.innerText = weapons[currentWeapon].power;
+        power = weapons[currentWeapon].power;
     }
     else if(gold>=40)
     {
@@ -108,6 +132,7 @@ function buyweapon()
         currentWeapon = 2
         stat.innerText = weapons[currentWeapon].name;
         p.innerText = weapons[currentWeapon].power;
+        power = weapons[currentWeapon].power;
     }
     else if(gold>=20)
     {
@@ -116,9 +141,10 @@ function buyweapon()
         g.innerText = gold
         stat.innerText = weapons[currentWeapon].name;
         p.innerText = weapons[currentWeapon].power;
+        power = weapons[currentWeapon].power;
     }
     else
-        text.innerText = "Not enough Gold!"
+        text.innerText = "Not enough Gold to buy a Weapon!"
 }
 
 function goCave()
@@ -135,19 +161,35 @@ function goCave()
 
 function diff()
 {
-    if(xp==0)
-    hard.style.display = "inline"
-    else if(xp>0 && xp<=10)
-    insane.style.display = "inline"
-    else if(xp>10 && xp<=30)
-    hard.style.display = "inline"
+    let fact = power/(m[mons].mh) + xp/100;
+    if(fact<0.50)
+    {
+        hard.style.display = "none"
+        easy.style.display = "none"
+        insane.style.display = "inline"
+        
+    }
+    else if(fact>=0.50 && fact<0.75)
+    {
+        insane.style.display = "none"
+        easy.style.display = "none"
+        hard.style.display = "inline"
+        
+    }
     else
-    easy.style.display = "inline"
+    {
+        insane.style.display = "none"
+        hard.style.display = "none"
+        easy.style.display = "inline"
+        
+    }
 }
 
 function fightSlime()
 {
+    mons = 0;
     hps = 50;
+    def=0;
     button2.innerText = "Attack"
     button3.innerText = "Dodge"
     text.innerText = "Fighting Slime...\n";
@@ -161,10 +203,107 @@ function fightSlime()
 
 function fightBeast()
 {
-
+    mons = 1;
+    hpb = 100;
+    bef = 0;
+    button2.innerText = "Attack"
+    button3.innerText = "Dodge"
+    text.innerText = "Fighting Beast...\n";
+    slime.style.display = "block"
+    diff();
+    hp1.innerText = hpb;
+    button1.onclick =  goCave;
+    button2.onclick = attack;
+    button3.onclick = dodge;
 }
 
 function fightDragon()
+{
+    mons = 2;
+    hpd = 200;
+    man = 0;
+    button1.innerText = '\u2190';
+    button2.innerText = "Attack"
+    button3.innerText = "Dodge"
+    text.innerText = "Fighting the Dragon...\n";
+    slime.style.display = "block"
+    diff();
+    hp1.innerText = hpd;
+    button1.onclick =  home;
+    button2.onclick = attack;
+    button3.onclick = dodge;
+}
+
+function attack()
+{
+    
+    let damage = Math.floor(Math.random()*11) + Math.floor((xp/10)*Math.random()) + Math.floor(power/5+(power/10)*Math.random());
+    if(mons==0 && hps-damage>0 && def==0)
+    {
+        text.innerText += "\nDamage dealt: " + damage;
+        hps-=damage;
+        hp1.innerText = hps;
+    }
+    else if(mons==0 && def==0)        // XP and Gold rewards for Slime
+    {
+        hps = 0;
+        hp1.innerText = hps;
+        text.innerText = "Congratulations, you beat the " + m[mons].name + "!\nThe Monster died and dropped "
+        t = Math.floor(15+Math.random()*11);
+        temp = Math.floor(15+Math.random()*11);
+        text.innerText += " " + t + " XP and " + temp + " Gold.";
+        xp += t;
+        gold += temp;
+        g.innerText = gold;
+        x.innerText = xp;
+        def++;
+    }
+    else if(mons==1 && hpb-damage>=0 && bef==0)
+    {
+        text.innerText += "\nDamage dealt: " + damage;
+        hpb-=damage;
+        hp1.innerText = hpb;
+    }
+    else if(mons==1 && bef==0)        // XP and Gold rewards for Beast 
+    {
+        text.innerText += "\nDamage dealt: " + damage;
+        hpb = 0;
+        hp1.innerText = hpb;
+        text.innerText = "Congratulations, you beat the " + m[mons].name + "!\nThe Monster died and dropped "
+        t = Math.floor(25+Math.random()*11);
+        temp = Math.floor(25+Math.random()*11);
+        text.innerText += " " + t + " XP and " + temp + " Gold.";
+        xp += t;
+        gold += temp;
+        g.innerText = gold;
+        x.innerText = xp;
+        bef++;
+    }
+    else if(mons==2 && man==0 && hpd-damage>=0)
+    {
+        text.innerText += "\nDamage dealt: " + damage;
+        hpd-=damage;
+        hp1.innerText = hpd;
+    }
+    else if(mons==2 && man==0)               // XP and rewards for the Dragon
+    {
+        text.innerText += "\nDamage dealt: " + damage;
+        hpd = 0;
+        hp1.innerText = hpd;
+        text.innerText = "Congratulations, you beat the DRAGON!!!\nThe Monster died and dropped "
+        t = Math.floor(40+Math.random()*11);
+        temp = Math.floor(40+Math.random()*11);
+        text.innerText += " " + t + " XP and " + temp + " Gold.\nYOU WIN!";
+        xp += t;
+        gold += temp;
+        g.innerText = gold;
+        x.innerText = xp;
+        man++;
+    }
+    
+}
+
+function dodge()
 {
 
 }
